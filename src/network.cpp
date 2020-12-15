d4673692b9a99bf0c89a3a21bb16c46d5f7a9148
Jachdich <jamesedkitching@gmail.com>
2020-12-15 17:00:12 +0000

    swap i & j somewhere and now it works

--- src/network.cpp
+++ src/network.cpp
@@ -80,7 +80,6 @@ void handleNetworkPacket(Json::Value root, SectorCache * cache) {
         	Sector * sec = cache->getSectorAt(loc.sectorX, loc.sectorY);
         	Star * s = &sec->stars[loc.starPos];
             Planet * p = &s->planets[loc.planetPos];
-            std::cout << res["result"] << "\n";
             PlanetSurface * surf = new PlanetSurface(res["result"], p);
             p->surface = surf;
 
